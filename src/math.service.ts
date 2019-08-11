3dd5ff891fe9bb6e2997536e15ad47eb695a8790
Brian Johnson <b.johnson@fan-gene.com>
2019-08-11 17:37:37 -0400

    finish

--- src/math.service.ts
+++ src/math.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { ClientProxyFactory, Transport, ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class MathService {
-    public accumulate(data: number[]): number {
-        return (data || []).reduce((a, b) => Number(a) + Number(b));
-    }
+  private client: ClientProxy;
+
+  constructor() {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 8877,
+      },
+    });
+  }
+
+  public accumulate(data: number[]) {
+    return this.client.send<number, number[]>('add', data);
+  }
 }
